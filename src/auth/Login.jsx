b3353d71431b6cc6fcfcf3f188aa4305a0fe4622
Isaac Correa <correayaguaranisaac@gmail.com>
2024-03-03 16:25:46 -0400

    login y registros completado_1

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import Logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
   return (
     <>
       <div className='img-container'>
-        <div> 
-            <img src={ Logo } alt='Logo' />
+        <div > 
+            <img height="75px" src={ Logo } alt='Logo' />
         </div>
-        <h1>Suministros C.A</h1>
-      </div>  
-
+          <h1>Suministros C.A</h1>  
+        </div>  
+        <div className='info-pagina'>
+        <p><strong>Suminstros C.A </strong> es una aplicacion en donde los administradores y vendedores manejan la logistica necesaria para vender los productos de una empresa mediante recepcion y cobro de pedidos.</p>
+        
+        </div>
       <section>
               <div className='image-container'>
-
+                  <img src={Logo} alt="" />
               </div>
               
               <div className='form-container'>
-                  <h3>Inicia Sesion</h3>
+                  <h2>Inicia Sesion</h2>
                   <form className='form-login ' >
                       <input type="email" placeholder='correo' />
                       <input type="password" placeholder='contraseña' />
-                      <p>¿No tienes cuenta? <span>registrate</span></p>
-                      <button type="submit">ingresar</button>
-
-
+                      <p>¿Quieres trabajar con nosotros?  <Link className='link'  to="/registro" ><span>postulate</span></Link> </p>
+                      <button type="submit">Ingresar</button> 
                   </form>
               </div>
       </section>
--- src/auth/Registro.jsx
+++ src/auth/Registro.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Logo from "../assets/logo.png"
 export const Registro = () => {
   return (
     <>
-        <h1>Registro</h1>
+      <div className='img-container'>
+        <div > 
+            <img height="75px" src={ Logo } alt='Logo' />
+        </div>
+          <h1>Suministros C.A</h1>  
+        </div>  
+        <div className='info-pagina'>
+        <p><strong>En Suministro CA, estamos buscando personas talentosas y motivadas para unirse a nuestro equipo de vendedores. Ofrecemos una oportunidad única para desarrollar tu carrera profesional en un entorno dinámico y desafiante, donde podrás aprender de los mejores y alcanzar tus metas. </strong></p>
+        
+        </div>
+      <section>
+              <div className='image-container'>
+                  <img src={Logo} alt="" />
+              </div>
+              
+              <div className='form-container'>
+                  <h2>¡Postulate!</h2>
+                  <form className='form-login ' >
+                      <input type="text" placeholder='nombre' />                    
+                      <input type="email" placeholder='correo' />
+                      <input type="tel" placeholder='telefono' />
+                      <p>¿Ya eres vendedor ?  <Link className='link'  to="/login" ><span>ingresa</span></Link> </p>
+                      <button type="submit">Enviar</button> 
+                  </form>
+              </div>
+      </section>
+        
     </>
   )
 }
