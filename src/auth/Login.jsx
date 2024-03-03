89fd4c814ba9820907dca122490fb44f57497ce6
Isaac Correa <correayaguaranisaac@gmail.com>
2024-03-02 20:52:41 -0400

    Estilos Iniciales

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -15,6 +15,14 @@ export const Login = () => {
               
               <div className='form-container'>
                   <h3>Inicia Sesion</h3>
+                  <form className='form-login ' >
+                      <input type="email" placeholder='correo' />
+                      <input type="password" placeholder='contraseña' />
+                      <p>¿No tienes cuenta? <span>registrate</span></p>
+                      <button type="submit">ingresar</button>
+
+
+                  </form>
               </div>
       </section>
         
