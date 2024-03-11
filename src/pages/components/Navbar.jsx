44d7ce61e24d06484b5a6f52444822313e685e31
Isaac Correa <correayaguaranisaac@gmail.com>
2024-03-11 17:46:11 -0400

    navbar

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../../assets/logo.png"
+import { TiThMenu } from "react-icons/ti";
+import { IoMdClose } from "react-icons/io";
 
 export const Navbar = () => {
+    const [mostrarMenu, setMostrarMenu] = useState(false)
+    const onMenuMostrar = () => setMostrarMenu(!mostrarMenu);
+
+
   return (
+   <>
     <div className='nav-container'>
         <nav className='nav-vendedor'>
             <div className='logo-nav'>
@@ -12,15 +19,36 @@ export const Navbar = () => {
             <div className='enlaces-nav'>
                 <ul>
                     <li>Productos</li>
-                    <li>Cobranzas</li>
+                    <li>Cobranzas <span>5</span> </li>
                     <li>Historial</li>
+                    <div className='info-user-nav'>
+                        <span>Vendedor</span>
+                        <button type='button'>Logout</button>
+                    </div>
                 </ul>
+           
             </div>
+          
+        </nav>
+        {
+         mostrarMenu?(<IoMdClose className='nav-menu-boton' onClick={onMenuMostrar}/>)
+                    :(<TiThMenu className='nav-menu-boton' onClick={onMenuMostrar} />)   
+
+        }
+    </div>
+    <div className={`query-nav ${mostrarMenu ? { display: "block" } : { display: "none" }}`}
+         style={{ display: mostrarMenu ? "block" : "none" }}
+     >   
+        <ul>
+            <li>Productos</li>
+            <li>Cobranzas <span>5</span> </li>
+            <li>Historial</li>
             <div className='info-user-nav'>
                 <span>Vendedor</span>
                 <button type='button'>Logout</button>
             </div>
-        </nav>
+       </ul>
     </div>
+   </>
   )
 }
